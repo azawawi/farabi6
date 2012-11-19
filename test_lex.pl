0f6918aa3adc68fc3ea41ff422b5a0530a4db1fc
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2012-11-19 14:54:34 +0300

    Added lexing of arguments

--- test_lex.pl
+++ test_lex.pl
@@ -2,13 +2,18 @@
 
 use Modern::Perl;
 
-my $target = q{say "Hello world"};
+my $target;
+if(@ARGV) {
+	$target = join(' ', @ARGV);
+} else {
+	$target = q{say "Hello world";};
+}
 
 use Data::Printer;
 
 my @tokens;
 while (1) {
-    if ( $target =~ /\G(([\$\@\%])?[A-Za-z_]([A-Za-z_]|(\-[A-Za-z_]))+)/gc ) {
+    if ( $target =~ /\G(([\$\@\%])?[A-Za-z_]([A-Za-z_0-9]|(\-[A-Za-z_0-9]))+)/gc ) {
         push @tokens, [ 'IDENTIFIER', $1, pos($target) - length($1) ];
     }
     elsif ( $target =~ /\G(\".+\")/gc ) {
@@ -22,6 +27,9 @@ while (1) {
     }
     elsif ( $target =~ /\G(\s+)/gc ) {
         push @tokens, [ 'WHITESPACE', $1, pos($target) - length($1) ];
+    }
+    elsif ( $target =~ /\G(;)/gc ) {
+        push @tokens, [ 'SYMBOL', $1, pos($target) - length($1) ];
     }
     else {
         last;
