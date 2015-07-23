0db0bcefedd28c77ff6b868217ecc586efdb96e0
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2015-07-23 11:46:03 +0300

    Control main window creation from Perl 6

--- my_app/main.js
+++ my_app/main.js
@@ -22,15 +22,6 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
   console.log('Application open for business :)');
 
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1024, height: 768 /*, "node-integration": false*/});
-
-  // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
-
-  // Open the devtools.
-  //mainWindow.openDevTools();
-  
   // Create a JSON::RPC client object
   var rpc = require('./node_modules/node-json-rpc');
   var client = new rpc.Client({
@@ -48,13 +39,57 @@ app.on('ready', function() {
         console.log("error while connecting to Perl 6 JSON::RPC bridge")
       }
       else {
-        console.log("JSON::RPC bridge started with main Perl 6 process")
+        console.log("JSON::RPC bridge started with main Perl 6 process");
+
+        var result = res.result || {};
+        var width = result.width || 1024;
+        var height = result.height || 768;
+
+        // Create the browser window.
+        mainWindow = new BrowserWindow({
+          "width": width, 
+          "height": height
+        });
+
+        // and load the index.html of the app.
+        mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+        // Open the devtools.
+        if(result.openDevTools) {
+          mainWindow.openDevTools();
+        }
+
+        // Emitted when the window is closed.
+        mainWindow.on('closed', function() {
+          // Dereference the window object, usually you would store windows
+          // in an array if your app supports multi windows, this is the time
+          // when you should delete the corresponding element.
+          mainWindow = null;
+          
+          // Stop Perl process
+          client.call(
+            {
+              "jsonrpc": "2.0", "method": "stop", "params": {}, "id": id++
+            },
+            function (err, res) {
+              // Did it all work ? 
+              if (err) {
+                console.log("Error: " + err);
+              }
+              else {
+                console.log("Result is " + res.result);
+              }
+            }
+          );
+          
+        });
+
       }
     }
   );
 
   var id = 0;
-  
+
   ipc.on('sum', function(event, a, b) {
     client.call(
       {"jsonrpc": "2.0", "method": "sum", "params": {"a": a, "b": b}, "id": id++},
@@ -68,32 +103,6 @@ app.on('ready', function() {
         }
       }
     );
-
-  });
-  
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-    
-    // Stop Perl process
-    client.call(
-      {
-        "jsonrpc": "2.0", "method": "stop", "params": {}, "id": id++
-      },
-      function (err, res) {
-        // Did it all work ? 
-        if (err) {
-          console.log("Error: " + err);
-        }
-        else {
-          console.log("Result is " + res.result);
-        }
-      }
-    );
-    
   });
   
 });
